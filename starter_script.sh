68e43eb73848aa5f7078a21ca493102ef4db49d9
Avi Areman <avi.areman@gmail.com>
2023-07-31 13:57:07 -0400

    refactor: cleanup install script and add pretty things 🚀

--- starter_script.sh
+++ starter_script.sh
@@ -1,18 +1,55 @@
 #!/usr/bin/env bash
 
-echo "starting script"
+_print(){
+tput setaf 6
+    echo "================================"
+    echo ">>> $1"
+    echo "================================"
+    echo
+tput sgr0
 
-echo "installing packages"
-# Install packages 
-sudo apt update && sudo apt install  \
+}
+
+clear
+
+tput setaf 6
+cat <<'HERE'
+ ____  _   _ ____    ____            _
+|  _ \| | | / ___|  / ___| _   _ ___| |_ ___ _ __ ___
+| | | | |_| \___ \  \___ \| | | / __| __/ _ \ '_ ` _ \
+| |_| |  _  |___) |  ___) | |_| \__ \ ||  __/ | | | | |
+|____/|_| |_|____/  |____/ \__, |___/\__\___|_| |_| |_|
+                           |___/
+ ___           _        _ _
+|_ _|_ __  ___| |_ __ _| | | ___ _ __
+ | || '_ \/ __| __/ _` | | |/ _ \ '__|
+ | || | | \__ \ || (_| | | |  __/ |
+|___|_| |_|___/\__\__,_|_|_|\___|_|
+
+ ____                ____            _       _
+|  _ \ _ __ ___     / ___|  ___ _ __(_)_ __ | |_
+| |_) | '__/ _ \____\___ \ / __| '__| | '_ \| __|
+|  __/| | |  __/_____|__) | (__| |  | | |_) | |_
+|_|   |_|  \___|    |____/ \___|_|  |_| .__/ \__|
+                                      |_|
+
+HERE
+
+tput sgr0
+
+_print "Installing Packages"
+
+# Install packages
+sudo apt update && sudo apt install -y  \
         xclip \
         git
 
-echo "Initiating docker"
+_print "Initiating Docker"
 sudo groupadd docker
-sudo usermod -aG docker $USER
+sudo usermod -aG docker "$USER"
 
-echo "Setting up SSH"
+_print "Setting Up SSH"
+
 # Add SSH
 if [[ ! -f ~/.ssh/id_rsa ]]; then
     ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
@@ -30,10 +67,8 @@ else
 fi
 
 # Restart computer
-echo "Rebooting"
+_print "Rebooting"
 echo "You're computer needs to reboot itself in order to finish the set up process."
 echo "Please make sure that you save anything that you need to."
 read -p "Press enter to continue..."
 sudo reboot
-
-
